20c22470ba75223030d498e0af402b113899a6c2
galaktose <wmatifts@gmail.com>
2023-01-23 14:55:59 +0800

    added my name (cool!)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 // ********************************************************* 
 // Course: TCP1101 PROGRAMMING FUNDAMENTALS 
 // Year: Trimester 1, 2022/23 (T2215) 
-// Lab: TxxL 
-// Names: MEMBER_NAME_1 | MEMBER_NAME_2 | MEMBER_NAME_3 
-// IDs: MEMBER_ID_1 | MEMBER_ID_2 | MEMBER_ID_3 
-// Emails: MEMBER_EMAIL_1 | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 
-// Phones: MEMBER_PHONE_1 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
+// Lab: TT2L 
+// Names: WAN MUHAMMAD ATIF BIN TARAM SATIRAKSA| MEMBER_NAME_2 | MEMBER_NAME_3 
+// IDs: 1211103154 | MEMBER_ID_2 | MEMBER_ID_3 
+// Emails: 1211103154@student.mmu.edu.my | MEMBER_EMAIL_2 | MEMBER_EMAIL_3 
+// Phones: 011-10255127 | MEMBER_PHONE_2 | MEMBER_PHONE_3 
 // ********************************************************* 
 
 // TODO: Fill in the missing information above and delete this line.
